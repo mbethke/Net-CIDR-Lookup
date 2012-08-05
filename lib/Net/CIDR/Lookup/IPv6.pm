67ec4d7680ec3e4521e3fe2a1ac32e9c01579f83
Matthias Bethke <matthias@towiski.de>
2012-08-05 17:30:05 -0600

    _walk() is iterative now like the IPv4 version
    This one was actually the motivation for the change because long prefixes caused
    Deep Recursion warnings and were generally really inefficient.

--- lib/Net/CIDR/Lookup/IPv6.pm
+++ lib/Net/CIDR/Lookup/IPv6.pm
@@ -404,23 +404,47 @@ sub _addr2print { inet_ntop(AF_INET6, pack('N4', reverse $_[0]->Chunk_List_Read(
 # Walk the tree in depth-first LTR order
 sub _walk {
 	my ($node, $addr, $bits, $cb) = @_;
-	my ($a, $b) = @$node;
+	my ($l, $r);
+    my @node_stack = ($bits, $node);
+    #print "================== WALK ==================: ", join(':',caller),"\n"; 
+    while(defined($node = pop @node_stack)) {
+        $bits    = pop @node_stack;
+        #print "LOOP: stack size ".@node_stack."\n";
+        if(__PACKAGE__ eq ref $node) {
+            ($l, $r) = @$node;
+            #printf "Popped l=%s r=%s, bits=%d\n", ($l//'<undef>'), ($r//'<undef>'), $bits;
+            ++$bits;
 
-	++$bits;
-    # Check left side
-    $addr->Bit_Off(128 - $bits);
-	if(__PACKAGE__ eq ref $a) {
-		$a->_walk($addr, $bits, $cb);
-	} else {
-		defined $a and $cb->($addr, $bits, $a);
-	}
-    # Check right side
-    $addr->Bit_On(128 - $bits);
-	if(__PACKAGE__ eq ref $b) {
-		$b->_walk($addr, $bits, $cb);
-	} else {
-		defined $b and $cb->($addr, $bits, $b);
-	}
+            # Check left side
+            $addr->Bit_Off(128 - $bits);
+            if(__PACKAGE__ eq ref $l) {
+                #print "L: pushing node=$l, bits=$bits\n";
+                defined $r and push @node_stack, ($bits, $r);
+                push @node_stack, ($bits, $l);
+                #printf "L: addr=%032b (%s)\n", $addr, _int2dq($addr);
+                next; # Short-circuit back to loop w/o checking $r!
+            } else {
+                #defined $l and printf "L: CALLBACK (%s/%d) => %s\n", _int2dq($addr), $bits, $l;
+                defined $l and $cb->($addr, $bits, $l);
+            }
+        } else {
+            # There was a right-side leaf node on the stack that will end up in
+            # the "else" branch below
+            #print "Found leftover right leaf $node\n";
+            $r = $node;
+        }
+
+        # Check right side
+        $addr->Bit_On(128 - $bits);
+        if(__PACKAGE__ eq ref $r) {
+            #print "R: pushing node=$r, bits=$bits\n";
+            push @node_stack, ($bits, $r);
+            #printf "R: addr=%032b (%s)\n", $addr, _addr2print($addr);
+        } else {
+            #defined $r and printf "R: CALLBACK (%s/%d) => %s\n", _addr2prin($addr | 1 << 32-$bits), $bits, $r;
+            defined $r and $cb->($addr, $bits, $r);
+        }
+    }
 }
 
 # Split a chunk into a minimal number of CIDR blocks.
