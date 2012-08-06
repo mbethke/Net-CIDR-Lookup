7a2d7e157f064022c4d3b86f883132515259348b
Matthias Bethke <matthias@towiski.de>
2012-08-06 00:37:39 -0600

    Adapted to new API; simplified a bit

--- t/lib/Net/CIDR/Lookup/IPv6/Test.pm
+++ t/lib/Net/CIDR/Lookup/IPv6/Test.pm
@@ -18,7 +18,7 @@ sub check_methods : Test(startup => 8) {
     can_ok($t,'lookup');
     can_ok($t,'lookup_num');
     can_ok($t,'clear');
-    can_ok($t,'dump');
+    can_ok($t,'to_hash');
     can_ok($t,'walk');
 }
 
@@ -33,11 +33,11 @@ sub _needs_ipv6 : Test(setup) {
 }
 #-------------------------------------------------------------------------------
 
-sub add : Tests(5) {
+sub add : Tests(3) {
     my $self = shift;
     my $t = $self->{tree};
-    lives_ok(sub { $t->add('2001:db8::/32', 42) }, 'add() succeeded');
-    lives_ok(sub { $t->add('2002:db8::/31', 23) }, 'add() succeeded');
+    $t->add('2001:db8::/32', 42);
+    $t->add('2002:db8::/31', 23);
     is($t->lookup('2001:db8::1234'), 42, 'Block 2001:db8::/32 lookup OK');
     is($t->lookup('2002:db8:1::'), 23, 'Block 2002:db8::/31 lookup OK');
     is($t->lookup('::1'), undef, 'No result outside blocks');
@@ -51,106 +51,8 @@ sub add_range : Tests(4) {
     is($t->lookup('2002:cb8::abc'),  42, 'Range 2001:db8::--2002:db8::abc OK');
     is($t->lookup('1::ffff'), 23, 'Range 1::1234--1::1:2345 OK');
     is($t->lookup('f::'), undef, 'No result outside blocks');
-    my $h = $t->dump;
+    my $h = $t->to_hash;
     is(scalar keys %$h, 39, 'Range expansion: number of keys');
 }
 
 1;
-__END__
-
-sub collision : Tests(2) {
-    my $self = shift;
-    my $t = $self->{tree};
-    lives_ok(sub { $t->add('192.168.0.129/25', 42) }, 'add() succeeded');
-    isnt($t->add('192.168.0.160/31', 23), 1, 'add() failed as expected');
-    # TODO errstr?
-}
-
-sub benign_collision : Test(3){
-    my $self = shift;
-    my $t = $self->{tree};
-    lives_ok(sub { $t->add('192.168.0.129/25', 42) }, 'add() succeeded');
-    lives_ok(sub { $t->add('192.168.0.160/31', 42) }, 'add() succeeded');
-    is($Net::CIDR::Lookup::errstr, undef, 'Benign CIDR block collision');
-}
-
-sub merger : Tests(4) {
-    my $self = shift;
-    my $t = $self->{tree};
-    lives_ok(sub { $t->add('192.168.0.130/25', 42) }, 'add() succeeded');
-    is($t->add('192.168.0.0/25', 42),   1, 'add() succeeded');
-    my $h = $t->dump;
-    is(scalar keys %$h, 1, 'Merged block: number of keys');
-    my ($k,$v) = each %$h;
-    is($k, '192.168.0.0/24', 'Merged block: correct merged net block');
-}
-
-sub nonmerger : Tests(3) {
-    my $self = shift;
-    my $t = $self->{tree};
-    lives_ok(sub { $t->add('192.168.0.130/25', 42) }, 'add() succeeded');
-    is($t->add('192.168.0.0/25', 23),   1, 'add() succeeded');
-    my $h = $t->dump;
-    is(scalar keys %$h, 2, 'Unmerged adjacent blocks: correct number of keys');
-}
-
-sub equalrange : Tests(4) {
-    my $self = shift;
-    my $t = $self->{tree};
-    lives_ok(sub { $t->add('192.168.0.130/25', 1) }, 'add() succeeded');
-    lives_ok(sub { $t->add('192.168.0.130/25', 1) }, 'add() succeeded');
-    my $h = $t->dump;
-    is(0+keys %$h, 1, 'Got single block from two equal inserts');
-    is($h->{'192.168.0.128/25'}, 1, 'Got correct block');
-}
-
-sub subrange1 : Tests(4) {
-    my $self = shift;
-    my $t = $self->{tree};
-    lives_ok(sub { $t->add('192.168.0.1/24', 1) }, 'add() range succeeded');
-    lives_ok(sub { $t->add('192.168.0.1/25', 1) }, 'add() subrange succeeded');
-    my $h = $t->dump;
-    is(0+keys %$h, 1, 'Got single block from range followed by one of its immediate subranges');
-    is($h->{'192.168.0.0/24'}, 1, 'Got correct block');
-}
-
-sub subrange2 : Tests(4) {
-    my $self = shift;
-    my $t = $self->{tree};
-    lives_ok(sub { $t->add('192.168.0.1/24', 1) }, 'add() range succeeded');
-    lives_ok(sub { $t->add('192.168.0.1/28', 1) }, 'add() subrange succeeded');
-    my $h = $t->dump;
-    is(0+keys %$h, 1, 'Got single block from range followed by a small subrange');
-    is($h->{'192.168.0.0/24'}, 1, 'Got correct block');
-}
-
-sub superrange1 : Tests(4) {
-    my $self = shift;
-    my $t = $self->{tree};
-    lives_ok(sub { $t->add('192.168.0.128/25', 1) }, 'add() range succeeded');
-    is($t->add('192.168.0.0/24', 1), 1,   'add() superrange succeeded');
-    my $h = $t->dump;
-    is(0+keys %$h, 1, 'Got single block from range followed by its superrange');
-    is($h->{'192.168.0.0/24'}, 1, 'Got correct block');
-}
-
-sub superrange2 : Tests(4) {
-    my $self = shift;
-    my $t = $self->{tree};
-    lives_ok(sub { $t->add('192.168.160.128/25', 1) }, 'add() range succeeded');
-    lives_ok(sub { $t->add('192.168.160.0/20', 1) }, 'add() superrange succeeded');
-    my $h = $t->dump;
-    is(0+keys %$h, 1, 'Got single block from range followed by one of its higher superranges');
-    is($h->{'192.168.160.0/20'}, 1, 'Got correct block');
-}
-
-sub clear : Tests(2) {
-    my $self = shift;
-    my $t = $self->{tree};
-    lives_ok(sub { $t->add('192.168.0.129/25', 42) }, 'add() succeeded');
-    $t->clear;
-    is(scalar keys %{$t->dump}, 0, 'Reinitialized tree');
-}
-
-1;
-
